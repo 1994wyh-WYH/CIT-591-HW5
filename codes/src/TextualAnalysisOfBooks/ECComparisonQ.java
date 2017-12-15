39cf716bb114fdd32d85e7fc038f73b79b6f6dfd
1994wyh-WYH <33205420+1994wyh-WYH@users.noreply.github.com>
2017-12-15 14:24:03 -0500

    Update ECComparisonQ.java

--- codes/src/TextualAnalysisOfBooks/ECComparisonQ.java
+++ codes/src/TextualAnalysisOfBooks/ECComparisonQ.java
@@ -7,123 +7,24 @@ package TextualAnalysisOfBooks;
 public class ECComparisonQ {
 
 	public static void main(String[] args) {
-		String s="alice-in-wonderland.txt";
+		String[] textList={"alice-in-wonderland.txt","christmas-carol.txt",
+				   "huck-finn.txt", "les-mis.txt", "metamorphosis.txt", "my-man-jeeves.txt", "pride-prejudice.txt"
+				"tale-of-two-cities.txt", "tom-sawyer.txt"};
 		
-		TopQuotes q=new TopQuotes(s);
-		TopQuotesB qB=new TopQuotesB(s);
-		String[] cArr=q.topQuotes();
-		String[] cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
+		for(String s:textList){
+			TopQuotes q=new TopQuotes(s);
+			TopQuotesB qB=new TopQuotesB(s);
+			String[] cArr=q.topQuotes();
+			String[] cArrB=qB.topQuotes();
+			System.out.println("Txt sample: "+s);
+			System.out.println("The time for creating a new object for version A and B respectively are: "
 				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
+			System.out.println("The time for adding a new object for version A and B respectively are: "
 				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
+			System.out.println("The time for getting tops for version A and B respectively are: "
 				+q.gettGetTop()+", "+qB.gettGetTop());
+		}
 		
-		s="christmas-carol.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="huck-finn.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="les-mis.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="metamorphosis.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="my-man-jeeves.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="pride-prejudice.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="tale-of-two-cities.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
-		
-		s="tom-sawyer.txt";
-		q=new TopQuotes(s);
-		qB=new TopQuotesB(s);
-		cArr=q.topQuotes();
-		cArrB=qB.topQuotes();
-		System.out.println("Txt sample: "+s);
-		System.out.println("The time for creating a new object for version A and B respectively are: "
-				+q.gettCreate()+", "+qB.gettCreate());
-		System.out.println("The time for adding a new object for version A and B respectively are: "
-				+q.gettAdd()+", "+qB.gettAdd());
-		System.out.println("The time for getting tops for version A and B respectively are: "
-				+q.gettGetTop()+", "+qB.gettGetTop());
 
 	}
 
